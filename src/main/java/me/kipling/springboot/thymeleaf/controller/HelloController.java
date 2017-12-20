a655b614c97c41e311552e7d4efe1b8105c6deee
KiplingGit <zhangwei.168520@sohu.com>
2017-12-20 22:10:24 +0800

    M

--- src/main/java/me/kipling/springboot/thymeleaf/controller/HelloController.java
+++ src/main/java/me/kipling/springboot/thymeleaf/controller/HelloController.java
@@ -3,7 +3,9 @@ package me.kipling.springboot.thymeleaf.controller;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 这里使用@Controller,而不是@RestController注解
@@ -16,9 +18,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
+	/**
+	 * 
+	 * 由于使用thymeleaf的html5模板，所以我将index.html模板文件
+	 * 直接放到了/src/java/resources/templates目录下。然而这个目录并不是首页文件的默认目录，所以我们需要手动将应用根路径映射到
+	 * /src/java/resources/templates/index.html下。
+	 * 这个在spring-mvc的Controller下映射一下就可以了。
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
 	@RequestMapping(value = "/hello")
 	public String helloGet(Map<String, Object> map) {
-		 map.put("name", "HowieLi");
+		map.put("name", "HowieLi");
 		return "hello";
 	}
+	
+	/**
+     * 测试hello
+     * @return
+     */
+    @RequestMapping(value = "/hello11",method = RequestMethod.GET)
+    public String hello(Model model) {
+        model.addAttribute("name", "Dear");
+        return "hello";
+    }
 }
