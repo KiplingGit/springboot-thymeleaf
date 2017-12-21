25144b606649e41feb203cb2f6e9e31e56e482e3
KiplingGit <zhangwei.168520@sohu.com>
2017-12-21 15:05:32 +0800

    M

--- src/main/java/me/kipling/springboot/thymeleaf/controller/TemplateController.java
+++ src/main/java/me/kipling/springboot/thymeleaf/controller/TemplateController.java
@@ -3,9 +3,7 @@ package me.kipling.springboot.thymeleaf.controller;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 
